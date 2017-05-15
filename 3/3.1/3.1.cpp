9ae552992fbb90e0896d4421d0103482ff4396ba
Mattia Rombi <mattyro96@gmail.com>
2017-05-15 15:34:48 +0200

    Corretto es 3.1

--- 3/3.1/3.1.cpp
+++ 3/3.1/3.1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h> // libreria necessaria per usare fmod
 using namespace std;
 
 int main() {
@@ -17,21 +18,22 @@ int main() {
 		cout << "Scegli i due numeri con due fare l'operazione:" << endl;
 		cin >> a;
 		cin >> b;
-		if (opz = 1) {
+		if (opz == 1) { // L'operatore uguale (=) non va confuso con l'operatore equivalente (==)
 			cout << "La somma e' uguale a " << a + b << endl;
 		}
-		if (opz = 2) {
+		if (opz == 2) {
 			cout << "la differenza e' uguale a " << a - b << endl;
 		}
-		if (opz = 3) {
+		if (opz == 3) {
 			cout << "La moltiplicazione e' uguale a " << a*b << endl;
 		}
-		if (opz = 4 & b != 0) {
+		if (opz == 4 && b != 0) { // non confondere & con &&
 			double d = a / b;
-			double resto = a % b;
-			cout << "La divisione e' uguale a " << d << "e il suo resto e' pari a " << resto << endl;
+			// double resto = a % b; -> Non puoi fare il resto di 2 double con %, funziona solo con gli int
+			double resto = fmod(a, b); // funzione necessaria per ricavare il resto da 2 double o float
+			cout << "La divisione e' uguale a " << d << " e il suo resto e' pari a " << resto << endl;
 		}
-		if (opz = 4 & b = 0) {
+		if (opz == 4 && b == 0) {
 			cout << "La divisone è impossibile perchè il divisore è pari a 0." << endl;
 		}
 
