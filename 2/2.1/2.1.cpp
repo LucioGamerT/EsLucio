5822bfbef452ffce8c54f9f5add3c8c9edfdaabe
Luciano Sirio <lupolucio96@hotmail.it>
2017-05-15 11:40:54 +0200

    Esercitazione 2  done

---
+++ 2/2.1/2.1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a1 = 3;
+	int b1 = 4;
+	int somma1 = a1 + b1;
+	int differenza1 = a1 - b1;
+	int divisione1 = a1 / b1;
+	int moltiplicazione1 = a1 * b1;
+	cout << "I numeri sono " << a1 << " e " << b1 << endl;
+	cout << "La somma e' " << somma1 << endl;
+	cout << "La differenza e' " << differenza1 << endl;
+	cout << "La divisione e' " << divisione1 << endl;
+	cout << "La moltiplicazione e' " << moltiplicazione1 << endl << endl;
+
+	float a2 = 4.0f;
+	float b2 = 5.0f;
+	float somma2 = a2 + b2;
+	float differenza2 = a2 - b2;
+	float divisione2 = a2 / b2;
+	float moltiplicazione2 = a2 * b2;
+	cout << "I numeri sono " << a2 << " e " << b2 << endl;
+	cout << "La somma e' " << somma2 << endl;
+	cout << "La differenza e' " << differenza2 << endl;
+	cout << "La divisione e' " << divisione2 << endl;
+	cout << "La moltiplicazione e' " << moltiplicazione2 << endl << endl;
+
+	double a3 = 3.6;
+	double b3 = 6.0;
+	double somma3 = a3 + b3;
+	double differenza3 = a3 - b3;
+	double divisione3 = a3 / b3;
+	double moltiplicazione3 = a3 * b3;
+	cout << "I numeri sono " << a3 << " e " << b3 << endl;
+	cout << "La somma e' " << somma3 << endl;
+	cout << "La differenza e' " << differenza3 << endl;
+	cout << "La divisione e' " << divisione3 << endl;
+	cout << "La moltiplicazione e' " << moltiplicazione3 << endl << endl;
+	return 0;
+}
