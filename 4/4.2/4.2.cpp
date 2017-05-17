bfadae2908e8c54b4a9c03cb66ae808e64f67b39
Luciano Sirio <lupolucio96@hotmail.it>
2017-05-17 12:35:31 +0200

    modifiche al 3.2 fichelle

--- 4/4.2/4.2.cpp
+++ 4/4.2/4.2.cpp
@@ -7,14 +7,20 @@ int main() {
 	double somma = 0;
 	double media = 0;
 
-	cout << "Inserire la serie di numeri di cui si vuole calcolare la media matematica: ";
+	cout << "Inserire la serie di numeri di cui si vuole calcolare la media matematica:\n";
 	do {
+		cout << numvar + 1 << ": ";
 		cin >> i;
 		if (i != 0) {
 			numvar++;
 			somma += i;
 		}
-	} while (i != 0);
+		else {
+			if (numvar == 0) {
+				cout << "Devi inserire almeno un numero\n";
+			}
+		}
+	} while (i != 0 || numvar == 0);
 	media = somma / numvar;
 
 	cout << "La media numerica della serie inserita e' uguale a " << media << ". " << endl;
