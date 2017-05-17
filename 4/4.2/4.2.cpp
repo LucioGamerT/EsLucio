aef0c3c8fda53cbd59249223a00a985310114a25
Luciano Sirio <lupolucio96@hotmail.it>
2017-05-17 12:29:07 +0200

    4.2 done

--- 4/4.2/4.2.cpp
+++ 4/4.2/4.2.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+using namespace std;
 
-int main()
-{
+int main() {
+	double i;
+	int numvar = 0;
+	double somma = 0;
+	double media = 0;
+
+	cout << "Inserire la serie di numeri di cui si vuole calcolare la media matematica: ";
+	do {
+		cin >> i;
+		if (i != 0) {
+			numvar++;
+			somma += i;
+		}
+	} while (i != 0);
+	media = somma / numvar;
+
+	cout << "La media numerica della serie inserita e' uguale a " << media << ". " << endl;
 	return 0;
 }
