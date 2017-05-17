4af579e7d5cb8ee407b10987751e4080fb2ee8cd
Luciano Sirio <lupolucio96@hotmail.it>
2017-05-17 11:59:25 +0200

    4.1 done

--- 4/4.1/4.1.cpp
+++ 4/4.1/4.1.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+using namespace std;
 
-int main()
-{
-	return 0;
+int main() {
+	int b;
+	do {
+		int a;
+		int fattoriale = 1;
+		cout << "Inserire il numero intero positivo di cui si vuole calcolare il fattoriale: ";
+		cin >> a;
+
+		if (a < 0) {
+			cout << "Il numero inserito e' negativo per cui non e' possibile calcolarne il fattoriale." << endl;
+			return 0;
+		}
+		for (int i = a; i > 0; i--) {
+			fattoriale *= i;
+		}
+		cout << "\nIl fattoriale e' uguale a " << fattoriale << ". " << endl;
+
+		cout << "\nVuoi continuare a calcolare fattoriali? Se si' digita 1 altrimenti digita 2: ";
+		cin >> b;
+	} while (b == 1);
 }
