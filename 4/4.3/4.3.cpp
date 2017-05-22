ed3702c36cbd1503a4a9769133ff8a884fa560fd
Luciano Sirio <lupolucio96@hotmail.it>
2017-05-22 10:12:20 +0200

    4.3 fatto mado schifo

--- 4/4.3/4.3.cpp
+++ 4/4.3/4.3.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 
 int main() {
+	cout << "Inserisci un serie di numeri interi positivi e ti diro' quanti di questi sono pari!(max 100 numeri)" << endl;
+	int num[100];
+	int div = 2;
+	int numarr = 0;
+
+	for (int i = 0; i < 100; i++) {
+		cout << "elemento " << i + 1 << ": ";
+		cin >> num[i];
+		if (num[i] == -1) {
+			numarr = i;
+			break;
+		}
+	}
+	for (int o = 0; o < numarr; o++) {
+		if (num[o] % 2 == 0) {
+			cout << num[o] << endl;
+		}
+	}
 
 	return 0;
 }
---
+++ 4/4.3/4.3[Conflitto].cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	do {
+		cout<< "Inserisci un serie di numeri interi positivi e ti diro' quanti di questi sono pari!"
+	}
+	return 0;
+}
