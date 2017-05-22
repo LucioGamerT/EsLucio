38da0add71ce8bfc3fadbd5054cdde5e3af9bfda
Luciano Sirio <lupolucio96@hotmail.it>
2017-05-22 12:02:58 +0200

    4.5 done . Esercitazione 4 finita

--- 4/4.5/4.5.cpp
+++ 4/4.5/4.5.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+using namespace std;
 
-int main()
-{
+int main() {
+	int divisori[512];
+	int num = 0;
+
+	cout << "Divisori di un numero intero positivo:\n";
+	do {
+		cout << "Inserisci un numero: ";
+		cin >> num;
+		if (num < 0 || num == 0) {
+			cout << "\nIl numero deve essere positivo e diverso da zero!\n";
+		}
+	} while (num == 0 || num < 0);
+
+	cout << "\nI porcodii del tuo numero sono: ";
+	int a = 0;
+	for (int i = num; i > 0; i--) {
+		if (num%i == 0) {
+			divisori[a] = i;
+			cout << i;
+			if (i > 1) { 
+				cout << ", ";
+			}
+		}
+	}
+	cout << ".\n\n";
+
 	return 0;
 }
