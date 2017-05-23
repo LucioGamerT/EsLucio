4a8fce23d2568fd7283da9a16fdf97b44905eff2
MatrixDJ96 <mattyro96@gmail.com>
2017-05-23 11:01:12 +0200

    Aggiornato (da finire) es 5.3

--- 5/5.3/5.3.cpp
+++ 5/5.3/5.3.cpp
@@ -2,29 +2,35 @@
 using namespace std;
 
 int main() {
-	int a = 0;
-	int b = 0;
-	int r = 0;
-	char c = 0;
-	int mcd = 0;
-	int div = 0;
+	int a, b, r, mcd; // non è necessario inizializzare le variabili perchè tanto le imposti dopo
+	char c;
 
 	cout << "MCD di due numeri:\n";
 	do {
 		c = 0;
-		cout << "Inserisci la coppia di numeri di cui vuoi sapere l'MCD: "; cin >> a >> b;
+		cout << "Inserisci la coppia di numeri di cui vuoi sapere l'MCD: ";
+		cin >> a >> b;
 
 		if (b == 0) {
 			mcd = a;
 		}
-		else if (a%b == 0) {
-			mcd = b;
+		else {
+			if (a < b) {
+				int tmp = a;
+				a = b;
+				b = a;
+			}
+			r = a % b;
+			if (r == 0) {
+				mcd = b;
+			}
+			else {
+				while (r != 0) { // cosa era "&" ???
+					// TODO
+					r = a % b;
+				}
+			}
 		}
-	/*	while (a&b != 0) {
-			r = a%b;
-			b = a; r = b;
-			mcd = b;
-		}*/
 
 		cout << "MCD: " << mcd << "\n\n";
 		cout << "Vuoi ripetere l'operazione? Se si digita 1, in caso contrario digita 0.\nScelta: ";
