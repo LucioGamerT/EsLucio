8a4c8d647ac13dbc34647b992f8b45579ad7cba2
Luciano Sirio <lupolucio96@hotmail.it>
2017-06-14 08:41:03 +0200

    7.3 fatto, devo solo capire come togliere lo spazio da dove si toglieil carattere

--- 7/7.3/7.3.cpp
+++ 7/7.3/7.3.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+using namespace std;
 
-int main()
-{
+int main() {
+	char str[256];
+	char a;
+	int scelta = 0;
+
+	cout << "Eliminare un carattere da un stringa:\n";
+
+	do {
+		cout << "\nInserire una stringa:\n";
+		cin >> str;
+		cout << "\nScegliere il carattere da cancellare dalla stringa: ";
+		cin >> a;
+
+		for (int k = 0; k < strlen(str); k++) {
+			if (str[k] == a) {
+				str[k] = ' ';
+			}
+		}
+		cout << "\nLa nuova stringa e': " << str << ".";
+
+		cout << "\n\nVuoi ripetere l'operazione? Se si' digita 1 altrimenti digita 0: ";
+		cin >> scelta;
+	} while (scelta == 1);
+
+	cout << "\n\n";
+
 	return 0;
 }
