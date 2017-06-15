b3d755dae072f7a0bf9c3b9f5442b883a43e81c3
MatrixDJ96 <mattyro96@gmail.com>
2017-06-15 16:04:01 +0200

    Correzione dei cout/cin ambigui

--- 7/7.3/7.3.cpp
+++ 7/7.3/7.3.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 #include <string.h>
-using namespace std;
 
 int main() {
 	char str[256];
 	char a;
 	int scelta = 0;
 
-	cout << "Eliminare un carattere da un stringa:\n";
+	std::cout << "Eliminare un carattere da un stringa:\n";
 
 	do {
 		int rim = 0;
 
-		cout << "\nInserire una stringa: ";
-		cin.getline(str, 256, '\n');
-		cout << "Scegliere il carattere da cancellare dalla stringa: ";
-		cin >> a;
+		std::cout << "\nInserire una stringa: ";
+		std::cin.getline(str, 256, '\n');
+		std::cout << "Scegliere il carattere da cancellare dalla stringa: ";
+		std::cin >> a;
 
 		for (int k = 0; k < strlen(str); k++) {
 			if (str[k] == a) {
@@ -24,31 +23,32 @@ int main() {
 					if (str[i] == a) {
 						k = 0;
 					}
-				} rim++;
+				}
+				rim++;
 			}
 		}
 
 		if (rim == 0) {
-			cout << "\nCarattere non trovato.\nLa stringa rimane la stessa: "<<str;
+			std::cout << "\nCarattere non trovato.\nLa stringa rimane la stessa: " << str;
 		}
 		if (rim == 1) {
-			cout << "\nE' stato rimosso solo un carattere.";
-			cout << "\nLa nuova stringa e': " << str << ".";
+			std::cout << "\nE' stato rimosso solo un carattere.";
+			std::cout << "\nLa nuova stringa e': " << str << ".";
 		}
 		if (rim>1) {
-			cout << "\nSono stati rimossi " << rim << " caratteri.";
-			cout << "\nLa nuova stringa e': " << str << ".";
+			std::cout << "\nSono stati rimossi " << rim << " caratteri.";
+			std::cout << "\nLa nuova stringa e': " << str << ".";
 		}
 
-		cout << "\n\nVuoi ripetere l'operazione? Se si' digita 1 altrimenti digita 0: ";
-		cin >> scelta;
+		std::cout << "\n\nVuoi ripetere l'operazione? Se si' digita 1 altrimenti digita 0: ";
+		std::cin >> scelta;
 
 		if (scelta == 1) {
 			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		}
 	} while (scelta == 1);
 
-	cout << "\n\n";
+	std::cout << "\n\n";
 
 	return 0;
 }
