5905215c759f2b989c2130c431309a0576e48707
Luciano Sirio <lupolucio96@hotmail.it>
2017-07-13 14:18:27 +0200

    diocan non trovo scriviEsami. (8.1), 7.5 non guardare

--- 7/7.5/7.5.cpp
+++ 7/7.5/7.5.cpp
@@ -1,6 +1,59 @@
 #include <iostream>
+#include <string.h>
 
-int main()
-{
-	return 0;
+
+int main() {
+	char str[256];
+	int numeri[256];
+	int grandezza = 0;
+	int quantita = 0;
+
+/*	std::cout << "Quanti numeri ci sono nella tua stringa?\n";
+	std::cout << "\nInserisci la tua stringa: ";
+	std::cin.getline(str, 256, '\n');
+	std::cout << "Inserisci ora il limite di numeri massimo che vuoi sapere: ";
+	std::cin >> grandezza;
+
+	std::cout << "\n( ";
+	for (int i = 0; i < grandezza; i++) {
+		numeri[i] = 0;
+		if (i < grandezza - 1) {
+			std::cout << numeri[i] << ", ";
+		}
+		else {
+			std::cout << numeri[i] << " ";
+		}
+	}
+
+
+	for (int i=0;i<grandezza;i++) {
+
+
+
+
 }
+
+	std::cout << ")= " << quantita;
+	std::cout << "\n\n";
+
+	*/
+
+
+	std::cout << "test: ";
+	std::cin.getline(str, 256, '\n');
+
+	for (int i = 0; str[i] != 5; i++) {
+		if (str[i]=='1') {
+			quantita++;
+		}
+	}
+
+	std::cout << quantita << "\n\n";
+
+
+
+
+
+
+	return 0;
+}  
--- 8/8.1/8.1.cpp
+++ 8/8.1/8.1.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <string.h>
 
-int main()
-{
-	return 0;
+void titolo_esercitazione() {
+	std::cout << "Sessione d'esami:\n\n";
+}
+
+struct esame {
+	char nome[32];
+	char cognome[32];
+	int matricola;
+	int voto;
+} esami[5];
+
+int main() {
+	int n;
+	for (n = 1; n < 6; n++) {
+		std::cout << "Esame num." << n << ":\n";
+		std::cout << "Nome: ";
+		std::cin >> esami[n].nome;
+		std::cout << "Cognome: ";
+		std::cin >> esami[n].cognome;
+		std::cout << "Numero di matricola: ";
+		std::cin >> esami[n].matricola;
+		std::cout << "Voto in trentesimi: ";
+		std::cin >> esami[n].voto;
+		std::cout << "\n";
+	}
+
+	std::cout << "\n--------------------------------------\n\nRiepilogo sessione d'esami:\n";
+	for (n = 1; n < 6; n++) {
+		std::cout << "Esame num." << n << ":\n";
+		scriviEsami(esami[n]);
+		return 0;
+	}
+}
+
+void scriviEsami(esame esami) {
+
+	std::cout << esami.nome << " " << esami.cognome << " (" << esami.matricola << "): " << esami.voto << "\n\n";
 }
